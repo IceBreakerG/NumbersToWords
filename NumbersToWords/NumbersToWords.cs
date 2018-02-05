bb56447dac39c610efdaca8cbf2322eb14e30e29
Eric Garrison <eric.garrison@theseam.com>
2018-02-05 11:25:12 -0600

    Initial commit with refactoring.

---
+++ NumbersToWords/NumbersToWords.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumbersToWords
+{
+	public static class NumbersToWords
+	{
+		private static List<string> Numerals = new List<string>() { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+		private static List<string> Positions = new List<string>() { "Hundred", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion", "Nonillion", "Decillion" };
+		private const string DoubleFixedPoint = "###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###,###.00";
+
+		/// <summary>
+		/// Converts a number into its gramatical representation.
+		/// </summary>
+		/// <param name="number">The number to convert.</param>
+		/// <param name="includeDollarsAndCents">Includes the word "Dollars" and, if there are any, the cents (ie. 19/100) at the end of the string.</param>
+		/// <returns>The gramatical representation of the specified number (ie. If 777, Seven Hundred Seventy Seven).</returns>
+		public static string ConvertNumberToWords(string number, bool includeDollarsAndCents = true)
+		{
+			var sb = new StringBuilder();
+			var DecimalValue = number.Split('.')[1];
+			var DigitGroup = number.Remove(number.IndexOf('.')).Split(',');
+			var DigitGroupLength = DigitGroup.Length;
+
+			if (number == "0.00")
+				return $"{GetNumeral(0)} Dollars";
+
+			for (int index = 0; index < DigitGroupLength; ++index)
+			{
+				int Digit = int.Parse(DigitGroup[index]);
+				int LeadingDigit = Digit / 100;     //ie. #__
+				int DigitRemnant = Digit % 100;     //ie. _##
+
+				if (LeadingDigit > 0 || DigitRemnant > 0)
+				{
+					if (Digit >= 100)
+					{
+						sb.Append($"{GetNumeral(LeadingDigit)} ");
+						sb.Append($"{GetPosition(0)} ");
+
+						if (DigitRemnant > 20)
+						{
+							int Tens = DigitRemnant / 10;   //ie. _#_
+							int Ones = DigitRemnant % 10;   //ie. __#
+
+							//The index for the numeral is 20 + the leading digit - 2 since the index for Twenty is 20, Thirty is 21, etc.
+							sb.Append($"{GetNumeral(20 + (Tens - 2))} ");
+
+							if (Ones > 0)
+								sb.Append($"{GetNumeral(Ones)} ");
+						}
+						else
+						{
+							if (DigitRemnant > 0)
+								sb.Append($"{GetNumeral(DigitRemnant)} ");
+						}
+					}
+					else
+					{
+						if (DigitRemnant > 20)
+						{
+							int one = DigitRemnant / 10;
+							int two = DigitRemnant % 10;
+
+							//The index for the numeral is 20 + the leading digit - 2 since the index for Twenty is 20, Thirty is 21, etc.
+							sb.Append($"{GetNumeral(20 + (one - 2))} ");
+
+							if (two > 0)
+								sb.Append($"{GetNumeral(two)} ");
+						}
+						else
+						{
+							if (DigitRemnant > 0)
+								sb.Append($"{GetNumeral(DigitRemnant)} ");
+						}
+					}
+
+					//Gets the Position of the number
+					if (DigitGroupLength > 1 && DigitGroupLength - index > 1)
+						sb.Append($"{GetPosition(DigitGroupLength - index)} ");
+				}
+
+
+				//Gets the decimals remaining if there are any
+				if (DigitGroupLength - index == 1 && includeDollarsAndCents)
+				{
+					var Dollar = DigitGroup[0] == "1" ? "Dollar" : "Dollars";
+					sb.Append($"{Dollar} and {DecimalValue}/{100} ");
+				}
+			}
+
+			return sb.ToString();
+		}
+
+		/// <summary>
+		/// Returns the position of the current index (ie. Hundred, Thousand, Million, etc).
+		/// </summary>
+		/// <param name="index">The current index of the position to get.</param>
+		/// <returns>The position of the current index.</returns>
+		private static string GetPosition(int index)
+		{
+			return (index == 0) ? Positions[index] : Positions[index - 1];
+		}
+
+		/// <summary>
+		/// Returns the numeral name of the current number.
+		/// </summary>
+		/// <param name="number">The current number to retrieve the numeral name of.</param>
+		/// <returns>The numeral name of the current number.</returns>
+		private static string GetNumeral(int number)
+		{
+			return Numerals[number];
+		}
+	}
+}
---
+++ NumbersToWords/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace NumbersToWords
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool Escape = false;
+			string number = "";
+			string main = "";
+			decimal num = 0.00m;
+
+			//Test loop to test the output of the NumbersToWords class
+			while (main != "q" && main != "Q")
+			{
+				while (!Escape)
+				{
+					Console.Write("Enter the number you want to convert to words (or Press Q to quit): ");
+					number = Console.ReadLine();
+
+					if (number == "q" || number == "Q")
+					{
+						main = number;
+						Escape = true;
+					}
+					else
+					{
+						Escape = decimal.TryParse(number, out num);
+
+						Stopwatch sw = new Stopwatch();
+						sw.Start();
+
+						Console.WriteLine($"\n{NumbersToWords.ConvertNumberToWords(num.ToString("N2"), false)}\n");
+
+						sw.Stop();
+
+						Console.WriteLine($"Total Time: {sw.Elapsed.ToString()}");
+					}
+				}
+
+				Escape = false;
+			}
+
+			/*
+			 * Examples of different numbers.  Uncomment these if you would like to run them to test the output.
+			 */
+			//decimal n1 = 8000000.00m;
+			//decimal n2 = 78965.00m;
+			//decimal n3 = 147852.00m;
+			//decimal n4 = 789654123369.00m;
+			//decimal n5 = 4862159732587439.00m;
+			//decimal n6 = 1478523698574456788491.00m;
+			//decimal n7 = 2369857418959498984651546.00m;
+			//decimal n8 = 1472369857418959498984651546.00m;
+			//decimal n9 = 19956782369857418959498984651.00m;
+			//decimal n10 = 2369857418959498984651546.00m;
+			//decimal n11 = 79228162514264337593543950335.00m;
+
+			//Console.WriteLine($"\n{ConvertNumberToWords(num.ToString("N2"))}\n");
+			//Console.WriteLine($"\n{ConvertNumberToWords(n1.ToString("N2"))}\n");
+			//Console.WriteLine($"\n{ConvertNumberToWords(n2.ToString("N2"))}\n");
+			//Console.WriteLine($"\n{ConvertNumberToWords(n3.ToString("N2"))}\n");
+			//Console.WriteLine($"\n{ConvertNumberToWords(n4.ToString("N2"))}\n");
+			//Console.WriteLine($"\n{ConvertNumberToWords(n5.ToString("N2"))}\n");
+			//Console.WriteLine($"\n{ConvertNumberToWords(n6.ToString("N2"))}\n");
+			//Console.WriteLine($"\n{ConvertNumberToWords(n7.ToString("N2"))}\n");
+			//Console.WriteLine($"\n{ConvertNumberToWords(n8.ToString("N2"))}\n");
+			//Console.WriteLine($"\n{ConvertNumberToWords(n9.ToString("N2"))}\n");
+			//Console.WriteLine($"\n{ConvertNumberToWords(n10.ToString("N2"))}\n");
+			//Console.WriteLine($"\n{ConvertNumberToWords(n11.ToString("N2"))}\n");
+
+			Console.Write("Press any key to continue");
+			Console.ReadKey();
+		}
+	}
+}
